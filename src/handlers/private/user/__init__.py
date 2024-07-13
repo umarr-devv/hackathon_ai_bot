5a3fd6ca79a104cdab8d39864e6a6eae52c9f387
umarr <umarr.developer@gmail.com>
2024-07-13 12:21:01 +0600

    delete UsrerMiddleware from user handlers

--- src/handlers/private/user/__init__.py
+++ src/handlers/private/user/__init__.py
@@ -1,11 +1,10 @@
 from aiogram import Router
 from src.filters.chat import UserFilter
-from src.handlers.private.user import start
+from src.handlers.private.user import start, user_test
 from src.middlewares import UserMiddleware, ThrottlingMiddleware
 
 router = Router()
-router.message.middleware(UserMiddleware())
 router.message.middleware(ThrottlingMiddleware())
-router.callback_query.middleware(UserMiddleware())
 router.message.filter(UserFilter())
 router.include_router(start.router)
+router.include_router(user_test.router)
