abfab6f747a452b23086a3644cc49c3741db2b0f
umarr <umarr.developer@gmail.com>
2024-07-13 11:32:32 +0600

    update CheckBoxKeyboard class and add InformatedLeyboard

--- src/keyboards/chekbox_keyboard.py
+++ src/keyboards/chekbox_keyboard.py
@@ -3,8 +3,8 @@ from src.functions.string_values import answers
 
 
 class ChekBoxKeyboard:
-    url_buttons: list[types.InlineKeyboardButton]
-    next_button: types.InlineKeyboardButton
+    url_buttons: list[types.InlineKeyboardButton] = []
+    next_button: list[types.InlineKeyboardButton] = []
     prefix: str
     answers_key: str
 
@@ -21,6 +21,40 @@ class ChekBoxKeyboard:
             ]
             for index in range(1, len(answers[self.answers_key]) + 1)
         ]
+        buttons += self.url_buttons + self.next_button
         return types.InlineKeyboardMarkup(
             inline_keyboard=buttons
         )
+
+
+class InformatedKeyboard(ChekBoxKeyboard):
+    url_buttons = [
+        [types.InlineKeyboardButton(
+            text='❕ Информация про хакатон',
+            url='deliver.latoken.com/hackathon'
+        )
+        ],
+        [
+            types.InlineKeyboardButton(
+                text='❕ Информация про Латокен',
+                url='deliver.latoken.com/about'
+            )
+        ],
+        [
+            types.InlineKeyboardButton(
+                text='❕ Ссылка на #nackedmanagement',
+                url='coda.io/@latoken/latoken-talent/nakedmanagement-88'
+            )
+        ]
+    ]
+
+    next_button = [
+        [
+            types.InlineKeyboardButton(
+                text='➡️ Следующий Вопрос',
+                callback_data='next-informated'
+            )
+        ]
+    ]
+
+    answers_key = 'informated'
