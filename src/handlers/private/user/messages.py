2f38047cd19049d1f792f522d27c86c395be0bba
umarr <umarr.developer@gmail.com>
2024-09-26 14:09:32 +0600

    fix import

--- src/handlers/private/user/messages.py
+++ src/handlers/private/user/messages.py
@@ -2,7 +2,7 @@ from aiogram import Router, types
 from aiogram.filters import StateFilter
 from aiogram.utils.chat_action import ChatActionSender
 
-from src.service.openai import get_completetions
+from src.service.ai_client import get_completetions
 
 router = Router()
 
@@ -10,5 +10,5 @@ router = Router()
 @router.message(StateFilter(None))
 async def on_message(message: types.Message, bot):
     async with ChatActionSender.typing(bot=bot, chat_id=message.chat.id):
-        text = await get_completetions(message.from_user.first_name, message.text)
+        text = await get_completetions(message.text)
         await message.answer(text)
